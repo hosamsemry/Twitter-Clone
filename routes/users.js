d3387b55c4ca10a25620dc42348c21f85aa724ba
hosam <semryhosam@gmail.com>
2025-07-28 00:49:59 +0300

    Added routes for users

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const auth = require('../middlewares/authMiddleware');
+const { toggleFollow, getUserById, getAllUsers, createUser, getCurrentUserProfile, updateUser, deleteUser } = require('../controllers/userController');
+const User = require('../models/User');
+
+router.get('/', auth ,getAllUsers);
+
+router.get('/profile', auth, getCurrentUserProfile);
+
+router.post('/', auth, createUser)
+
+router.get('/:id', auth, getUserById);
+
+router.put('/:id', auth, updateUser);
+
+router.delete('/:id', auth, deleteUser);
+
+router.post('/:id/follow', auth, toggleFollow);
+
+module.exports = router;
