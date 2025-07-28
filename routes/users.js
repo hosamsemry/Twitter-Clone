80561887beb12fddc9b7196ff1a06a913ae2b58a
hosam <semryhosam@gmail.com>
2025-07-29 01:35:15 +0300

    Added the middleware to users routs

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,21 @@ const router = require('express').Router();
 const auth = require('../middlewares/authMiddleware');
 const { toggleFollow, getUserById, getAllUsers, createUser, getCurrentUserProfile,updateProfile, updateUser, deleteUser } = require('../controllers/userController');
 const User = require('../models/User');
+const admin = require('../middlewares/adminMiddleware')
 
-router.get('/', auth ,getAllUsers);
+router.get('/', auth, admin, getAllUsers);
 
 router.get('/profile', auth, getCurrentUserProfile);
 
 router.put('/profile', auth, updateProfile);
 
-router.post('/', auth, createUser)
+router.post('/', auth, admin, createUser)
 
-router.get('/:id', auth, getUserById);
+router.get('/:id', auth,admin, getUserById);
 
-router.put('/:id', auth, updateUser);
+router.put('/:id', auth,admin, updateUser);
 
-router.delete('/:id', auth, deleteUser);
+router.delete('/:id', auth,admin, deleteUser);
 
 router.post('/:id/follow', auth, toggleFollow);
 
