56998f0e0b5af88afc73fd23c9ec5e5add8fc307
hosam <semryhosam@gmail.com>
2025-07-29 01:18:37 +0300

    Added an endpoint for user profile update

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const auth = require('../middlewares/authMiddleware');
-const { toggleFollow, getUserById, getAllUsers, createUser, getCurrentUserProfile, updateUser, deleteUser } = require('../controllers/userController');
+const { toggleFollow, getUserById, getAllUsers, createUser, getCurrentUserProfile,updateProfile, updateUser, deleteUser } = require('../controllers/userController');
 const User = require('../models/User');
 
 router.get('/', auth ,getAllUsers);
 
 router.get('/profile', auth, getCurrentUserProfile);
+
+router.put('/profile', auth, updateProfile);
 
 router.post('/', auth, createUser)
 
