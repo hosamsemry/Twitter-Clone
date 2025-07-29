da87dcec2bc2f87e097a9552f7df91eaae4a4dbf
hosam <semryhosam@gmail.com>
2025-07-29 16:07:47 +0300

    added retweetOf field in tweet model

--- models/Tweet.js
+++ models/Tweet.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const tweetSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true, maxlength: 280 },
+  content: {type: String,  maxlength: 280,  required: function () {return !this.retweetOf;}},
   media: { type: String },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   likesCount: { type: Number, default: 0 },
+  retweetOf: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tweet',
+    default: null,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Tweet', tweetSchema);
