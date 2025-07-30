c9d1d11cd8797e224985f211e173e0e8cca32e0b
hosam <semryhosam@gmail.com>
2025-07-30 23:39:25 +0300

    Added bookmarks field to user model

--- models/User.js
+++ models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
   avatar:   { type: String, default: 'https://www.freepik.com/free-vector/illustration-businessman_2606517.htm#fromView=search&page=1&position=19&uuid=4fb07192-05b5-4eb7-b075-6b77b71fcabd&query=default+profile+image' },
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
