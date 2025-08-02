7329b1ab31624d9300451b5ebdaae3b518db4276
hosam <semryhosam@gmail.com>
2025-08-02 16:38:06 +0300

    Enabled cors middleware

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
